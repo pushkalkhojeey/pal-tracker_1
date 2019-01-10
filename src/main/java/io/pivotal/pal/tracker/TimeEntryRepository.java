d3f2387773b94203fff1485bc2d1e217dfc8ced5
dhiraj <dhiraj1403@gmail.com>
2019-01-10 11:34:32 +0530

    MVC-REST

--- src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
@@ -2,10 +2,12 @@ package io.pivotal.pal.tracker;
 
 import java.util.List;
 
-public interface TimeEntryRepository {
+public interface TimeEntryRepository
+{
     TimeEntry create(TimeEntry timeEntry);
     TimeEntry find(Long id);
     List<TimeEntry> list();
     TimeEntry update(Long id, TimeEntry timeEntry);
     void delete(Long id);
+
 }
